5bc941e8c6bc3f21c8c4146dad8b0d8ca7d2accc
Yuliya <iksulia1@gmail.com>
2024-04-19 17:50:52 +0300

    add accordion

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
-const swiper = new Swiper('.swiper', {
+const swiperHero = new Swiper('.hero__swiper', {
   effect: 'fade',
   autoplay: {
     delay: 3500,
     disableOnInteraction: false,
   },
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+});
+
+const swiperAbout = new Swiper('.about__swiper', {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  freeMode: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+//accordeon
+// document.querySelectorAll('.accordeon__triger').forEach(item => {
+//   item.addEventListener('click', () => {
+//     item.parentNode.classList.toggle('accordeon__item--active');
+//   });
+// });
+
+document.querySelectorAll('.accordion__triger').forEach(trigger => {
+  trigger.addEventListener('click', () => {
+    trigger.parentNode.classList.toggle('accordion__item--active');
+  });
 });
